a0ad341231f847116c3e6dd60ace3da0a0b11100
Ching Him Leung <cleung@jlab.org>
2026-07-14 10:03:43 -0400

    further increase max tracks
    Add error message in LADKine

--- src/THcLADGEM.h
+++ src/THcLADGEM.h
@@ -71,7 +71,7 @@ protected:
   void LoadCM();
   void RotateToLab(Double_t angle, TVector3 &vect);
 
-  static const Int_t MAXTRACKS = 1000; // Was 100
+  static const Int_t MAXTRACKS = 1500; // Was 100
 
   Int_t fNModules; // total number of modules
   Int_t fNLayers;  // total number of GEM layers
